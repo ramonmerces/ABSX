cc72175d6452ca2de2a8fe8800eab06f456a8497
Ramon Merces <ramon.merces@convergenceworks.com.br>
2021-04-26 15:21:43 -0300

    Fix: Sanctum auth no servidor

--- app/Models/Vendedor.php
+++ app/Models/Vendedor.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Sanctum\hasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
 class Vendedor extends Model
 {
-    use hasApiTokens, HasFactory;
+    use HasApiTokens, HasFactory;
     protected $guarded = []; 
 }
